769f6bc2fa067ef3ab013a3a16f14a8b1f7fa29d
Fernando Estevam <fernandoestevam23@gmail.com>
2025-06-30 22:12:28 -0300

    ajuste config husky, commitlint

---
+++ commitlint.config.ts
@@ -0,0 +1,27 @@
+// commintlin.confg.ts
+import type {UserConfig} from '@commitlint/types'
+
+const config: UserConfig = {
+  extends: ["@commitlint/config-conventional"],
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      [
+        "build",
+        "chore",
+        "ci",
+        "docs",
+        "feat",
+        "fix",
+        "perf",
+        "refactor",
+        "revert",
+        "style",
+        "test",
+      ]
+    ]
+  }
+}
+
+export default config
