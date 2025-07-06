285aacd8cd306daead3b9d464a62ab819f10f168
Fernando Estevam <fernandoestevam23@gmail.com>
2025-07-06 20:58:27 -0300

    feat: add class adjustment para create, update, delete, findById e findAll

---
+++ src/repositories/prisma/adjustment-repository.ts
@@ -0,0 +1,35 @@
+import { Adjustment } from "@prisma/client";
+import { BaseRepository } from "../base-repository";
+import { AdjustmentCreateInput, AdjustmentId, AdjustmentOutputResponseCreate, AdjustmentUpdateInput } from "@/schemas/adjustment.schema";
+import { FastifyInstance } from "fastify";
+
+export class PrismaAdjustmentRepository extends BaseRepository<
+Adjustment, 
+AdjustmentCreateInput, 
+AdjustmentUpdateInput,
+AdjustmentId> {
+  constructor(private app: FastifyInstance) {
+    super()
+  }
+
+  async findById(id: AdjustmentId): Promise<Adjustment | null> {
+    return await this.app.prisma.adjustment.findFirst({where: id})
+  }
+
+  async findAll(): Promise<Adjustment[]> {
+    return await this.app.prisma.adjustment.findMany();
+  }
+
+  async create(data: AdjustmentCreateInput): Promise<Adjustment> {
+   return  await this.app.prisma.adjustment.create({data})
+  }
+
+  async update(id: AdjustmentId, data: AdjustmentUpdateInput) {
+    return await this.app.prisma.adjustment.update({where:id, data})
+  }
+
+  async delete(id: AdjustmentId): Promise<void> {
+    await this.app.prisma.adjustment.delete({where: id})
+  }
+  
+}
