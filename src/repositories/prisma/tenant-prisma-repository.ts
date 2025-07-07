fc54967efaf6f69b513f1f85062ffc7a96ccaaa6
Fernando Estevam <fernandoestevam23@gmail.com>
2025-07-06 21:16:58 -0300

    feat: mudanças tenant prisma repository

--- src/repositories/prisma/tenant-prisma-repository.ts
+++ src/repositories/prisma/tenant-prisma-repository.ts
@@ -1,32 +1,32 @@
-import { FastifyInstance } from "fastify";
-import { TenantRepository } from "../tenant-repository";
-import { TenantInput, TenantInputIdSchema, TenantInputParamsSchema, TenantInputUpdate } from "../../schemas/tenant.schema";
-import { Tenant } from "@prisma/client";
+  import { FastifyInstance } from "fastify";
+  import { TenantRepository } from "../tenant-repository";
+  import { TenantInput, TenantInputIdSchema, TenantInputParamsSchema, TenantInputUpdate } from "../../schemas/tenant.schema";
+  import { Tenant } from "@prisma/client";
 
-export class PrismaTenantRepository implements TenantRepository {
-  
-  constructor(private app: FastifyInstance){}
-  async findById({id}: TenantInputIdSchema): Promise<Tenant | null> {
-    return await this.app.prisma.tenant.findUnique({ where: { id } })
-  }
+  export class PrismaTenantRepository implements TenantRepository {
+    
+    constructor(private app: FastifyInstance){}
+    async findById({id}: TenantInputIdSchema): Promise<Tenant | null> {
+      return await this.app.prisma.tenant.findUnique({ where: { id } })
+    }
 
-  async delete({id}:TenantInputIdSchema): Promise<void> {
-    await this.app.prisma.tenant.delete({ where: { id } })
-  }
+    async delete({id}:TenantInputIdSchema): Promise<void> {
+      await this.app.prisma.tenant.delete({ where: { id } })
+    }
 
-  async update({id}: TenantInputIdSchema, data: TenantInputUpdate): Promise<Tenant> {
-    return await this.app.prisma.tenant.update({ where: {id}, data })
-  }
+    async update({id}: TenantInputIdSchema, data: TenantInputUpdate): Promise<Tenant> {
+      return await this.app.prisma.tenant.update({ where: {id}, data })
+    }
 
-  async findByIdOrDocument(where: TenantInputParamsSchema) {
-    return await this.app.prisma.tenant.findFirst({ where })
-  }
+    async findByIdOrDocument(where: TenantInputParamsSchema) {
+      return await this.app.prisma.tenant.findFirst({ where })
+    }
 
-  async create(data: TenantInput) {
-    await this.app.prisma.tenant.create({ data })
-  }
+    async create(data: TenantInput) {
+      await this.app.prisma.tenant.create({ data })
+    }
 
-  async findAll() {
-    return await this.app.prisma.tenant.findMany() 
+    async findAll() {
+      return await this.app.prisma.tenant.findMany() 
+    }
   }
-}
